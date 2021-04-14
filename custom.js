f335b63a3d75a5d73514b73e05c7247d442843ff
Lawrence Gorris <gorrislawrence.personal@gmail.com>
2021-04-14 16:17:06 +0200

    Added custom JS effects

---
+++ custom.js
@@ -0,0 +1,15 @@
+$( ".card" ).hover(
+    function() {
+      $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+    }, function() {
+      $(this).removeClass('shadow-lg');
+    }
+  );
+
+  $( ".btn" ).hover(
+    function() {
+      $(this).addClass('rounded-pill btn-primary').css('cursor', 'pointer'); 
+    }, function() {
+        $(this).removeClass('btn-primary rounded-pill');
+      }
+  ); 
